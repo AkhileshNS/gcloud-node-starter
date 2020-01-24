fb0bc8def1e8a3e44f0b6de54894ad314999f861
AkhileshNS <nsakhilesh02@gmail.com>
2020-01-25 00:18:50 +0530

    added prebuild script in github actions

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import helmet from 'helmet';
 
 // VARIABLES
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; // When using flexible environments, keep in mind GCP will look for your server in ports 8080-8084
 
 // MIDDLEWARE
 app.use(compression());
